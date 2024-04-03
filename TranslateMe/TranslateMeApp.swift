e68cff4a9af873e9ffa8ec3f9c2fa4bec0e026e8
codeBender31 <ibarraisrael949@yahoo.com>
2024-04-03 14:29:21 -0500

    Initial Commit

---
+++ TranslateMe/TranslateMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslateMeApp.swift
+//  TranslateMe
+//
+//  Created by Bender on 4/3/24.
+//
+
+import SwiftUI
+
+@main
+struct TranslateMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
